9732ec67fb6c37808ada82f737611b8acdd4e178
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-08 23:44:43 +0600

    Backend: Install express mongodb and cors

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://afnanhussain2022:bMp4pSPnvaTfWj8@cluster0.vwpy9jc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    const todoCollection = client.db('todoListDB').collection('todo')
+
+    //    create a user
+    app.post('/todo', async(req,res)=>{
+        const todo = req.body
+        const result = await todoCollection.insertOne(todo)
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res) =>{
+    res.send('Hello World')
+})
+
+app.listen(port, () =>{
+    console.log(`Port is running on port ${port}`)
+})
