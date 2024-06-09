30587d4d752fc524083ab87f78f3d9cd2404035a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-09 13:01:22 +0600

    Client: Change the width of the todo list app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
    <main className="flex items-center justify-center min-h-screen bg-first-color">
-        <section className="w-1/2">
+        <section className="w-1/4">
           <div className="bg-second-color shadow-xl w-full font-bold py-3 text-center">
-            <h3 className="text-white text-2xl flex items-center gap-36"> <span className="px-2"><MdOutlineMenu/></span>Todo List App</h3>
+            <h3 className="text-white text-2xl flex justify-between items-center px-5"> <span className="px-2"><MdOutlineMenu/></span>Todo List App</h3>
           </div>
           <div className="bg-white shadow-xl mt-4 py-5 font-semibold grid justify-center text-gray-400 text-xl">
             <p className="flex items-center gap-4">First Todo <span><FaRegEdit/></span><span><MdDelete/></span></p>
