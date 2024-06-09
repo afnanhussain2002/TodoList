a31430ac5671ab056bebb9b78ab9d0da538a52a9
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-09 09:52:09 +0600

    Server: Seceure mongodb database username and password

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://afnanhussain2022:bMp4pSPnvaTfWj8@cluster0.vwpy9jc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwpy9jc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +26,21 @@ async function run() {
     await client.connect();
     const todoCollection = client.db('todoListDB').collection('todo')
 
-    //    create a user
+    // get all todo list
+    app.get('/todo', async(req,res) =>{
+      const allTodo = todoCollection.find()
+      const result = await allTodo.toArray() 
+      res.send(result)
+    })
+
+    //  get a single todo
+    app.get('/todo/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await todoCollection.findOne(query)
+      res.send(result)
+    })
+    //  create a todo
     app.post('/todo', async(req,res)=>{
         const todo = req.body
         const result = await todoCollection.insertOne(todo)
@@ -36,7 +51,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
